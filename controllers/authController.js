f4318c34d96a9b719ae131005689437ed79096d0
durga0401 <stromboy004@gmail.com>
2026-02-24 00:01:19 +0530

    Add fallback for JWT_EXPIRES_IN

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,7 +11,7 @@ const generateToken = (user) => {
   return jwt.sign(
     { id: user.id, email: user.email, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRES_IN },
+    { expiresIn: process.env.JWT_EXPIRES_IN || "7d" },
   );
 };
 
